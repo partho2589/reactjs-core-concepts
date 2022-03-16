3ab9a57b0c50387f6a2f2f78b73128b07afb0cf9
partho2589 <parthomalo615@.com>
2022-03-16 16:44:02 +0600

    added all files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 55555;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1> 
+        <div className='container'>
+          <h3>Holle Dude. React. How are you.</h3>
+        </div>
+        <div className="music">
+          <p> Name: {}</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
