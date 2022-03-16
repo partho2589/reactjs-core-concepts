0a340c41b4dfbb44f9300fe82dc915369aeb5c39
partho2589 <parthomalo615@.com>
2022-03-17 00:41:13 +0600

    components

--- src/App.js
+++ src/App.js
@@ -6,16 +6,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <Person> </Person>
-      <friend></friend>
+     <Person name = "Pronto" job = "student"> </Person>
+     <Person name = "Ruble" job = "student"> </Person>
+     <Person name = 'Maruf Ali' job= "student"> </Person>
+           <h1>Well come React.</h1>
+     <Friend name = 'Rana' phone ="01721915"></Friend>
+     <Friend name = 'saila' phone = '014541'></Friend>
+     <Friend></Friend>
+      
     </div>
   );
 };
-function Person (){
+function Person (props){
+  console.log(props)
   return (
     <div className="person">
-      <h1> Partho malo</h1>
-      <p>Profession: Web developer</p>
+      <h1>Name: {props.name}</h1>
+      <p>Job: {props.job}</p>
+    </div>
+  )
+};
+function Friend (props){
+  return (
+    <div className='friend'>
+      <h3>Name: {props.name}</h3>
+      <p>Phone:{props.phone} </p>
     </div>
   )
 }
