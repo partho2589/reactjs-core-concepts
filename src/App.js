6e527dd761f3dd6bd389e3333e79f5e83eeb5b39
partho2589 <parthomalo615@.com>
2022-03-17 00:09:04 +0600

    added first component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-const number = 55555;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1> 
-        <div className='container'>
-          <h3>Holle Dude. React. How are you.</h3>
-        </div>
-        <div className="music">
-          <p> Name: {}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Person> </Person>
+      <friend></friend>
     </div>
   );
+};
+function Person (){
+  return (
+    <div className="person">
+      <h1> Partho malo</h1>
+      <p>Profession: Web developer</p>
+    </div>
+  )
 }
+
 
 export default App;
